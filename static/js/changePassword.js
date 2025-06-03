ad80a0cd29602dc8d93cbc442b46afa8d8f8d843
KhemrajSingh Rathore <khemraj.rathore@privado.ai>
2025-06-03 10:29:21 +0530

    Update changePassword.js

--- static/js/changePassword.js
+++ static/js/changePassword.js
@@ -1,6 +1,7 @@
 function validate() {
     var pass = document.getElementById("newpassword").value;
     var cpass = document.getElementById("cpassword").value;
+    var passportNumber = document.getElementById("passportNo").value;
     if (pass == cpass) {
         return true;
     } else {
